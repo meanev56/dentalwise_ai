f3f1d138465686da4788affcf05de24117ec0650
meanev56 <emekanjoku86@gmail.com>
2026-01-05 15:34:00 +0100

    Refactor code structure for improved readability and maintainability

--- components/landing/Header.tsx
+++ components/landing/Header.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { SignInButton, SignUpButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
 
-const Header = () => {
+function Header () {
   return (
-    <div>Header</div>
+    <nav className="fixed top-0 right-0 left-0 z-50 px-6 py-2 border-b border-border/50 bg-background/80 backdrop-blur-md h-16">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="flex items-center gap-2"
+        >
+          <Image
+            src="/logo.png"
+            alt="DentalWise AI Logo"
+            width={32}
+            height={32}
+          />
+          <span className="font-semibold text-lg">Dentalwise AI</span>
+        </Link>
+
+        <div className="hidden md:flex items-center gap-8">
+          <a 
+            href="#"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            How it Works
+          </a>
+          <a 
+            href="#"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            Pricin
+          </a>
+          <a 
+            href="#"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            About
+          </a>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <SignInButton mode="modal">
+            <Button variant={"ghost"} size={"sm"}>
+              Login
+            </Button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <Button size={"sm"}>
+              Sign Up
+            </Button>
+          </SignUpButton>
+        </div>
+      </div>
+    </nav>
   )
 }
 
